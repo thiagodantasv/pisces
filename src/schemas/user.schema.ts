e976f023f9a86cfe7cea53cf8dfdc4c91428c773
thiagodantasv <thiagodantasv@gmail.com>
2020-10-12 12:33:52 -0300

    user schema creation

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type userSchema = User & Document;
+
+@Schema()
+export class User{
+    @Prop({required: true})
+    name: string;
+
+    @Prop({required: true})
+    email: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
