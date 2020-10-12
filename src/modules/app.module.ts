71005f964118bd236527c2631f835d07230db705
thiagodantasv <thiagodantasv@gmail.com>
2020-10-12 14:12:16 -0300

    including the mongo module importation in root module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoModule } from './mongo.module';
 import { UserModule } from './user.module';
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongoModule
+  ],
   controllers: [],
   providers: [],
 })
