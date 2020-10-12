4c508951ab9bbdf938f8c084f289cda16b46a1e1
thiagodantasv <thiagodantasv@gmail.com>
2020-10-12 12:41:16 -0300

    creating mongo document based on user schema

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './../controllers/user.controller';
 import { UserService } from './../services/user.service';
+import { User, UserSchema } from './../schemas/user.schema';
 
 @Module({
-  imports: [],
+  imports: [
+      MongooseModule.forFeature([
+          { name: User.name, schema:UserSchema }
+      ])
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
