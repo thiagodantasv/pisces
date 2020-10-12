7323d7bc10df929b7e08541693afa654a21a6f0c
thiagodantasv <thiagodantasv@gmail.com>
2020-10-12 14:20:40 -0300

    createUser api basic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from '../services/user.service';
+import { CreateUserDto } from './../dto/create-user.dto';
+import { User } from './../schemas/user.schema';
 
 @Controller(`/user`)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post(`/create`)
-  createUser(): string {
-    return this.userService.createUser();
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUserDto);
   }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from 'src/schemas/user.schema';
+import { User, UserDocument } from './../schemas/user.schema';
+import { CreateUserDto } from './../dto/create-user.dto';
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
 
-  createUser(): string {
-    return 'create user';
+  async createUser(newUser: CreateUserDto): Promise<User> {
+    console.log(`Creating a new user: \n${JSON.stringify(newUser)}`);
+    const createdUser = await new this.userModel(newUser);
+    return createdUser.save();
   }
 }
